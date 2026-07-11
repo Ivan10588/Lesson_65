c4ae640232d0c64bdcf41cfc8725e4a54819c70b
Ivan10588 <Ivanov_Ivan_88@mail.ru>
2026-07-11 14:53:46 +0700

    Lesson_65

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ class Item(BaseModel):
     description: Optional[str] = None
     price: float
     tax: Optional[float] = 0.0
+
+from fastapi import FastAPI, HTTPException
+from typing import List
+
+app = FastAPI()
+
+items_db: List[Item] = []
+
+@app.get("/items/", response_model=List[Item])
+def get_items():
+    return items_db
+
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int):
+    for item in items_db:
+        if item.id == item_id:
+            return item
+    raise HTTPException(status_code=404, detail="Item not found")
